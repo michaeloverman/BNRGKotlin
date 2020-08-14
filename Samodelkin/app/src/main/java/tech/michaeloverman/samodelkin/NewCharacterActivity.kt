faf3a8bac3f81b85735da4f008a335967defe352
Michael Overman <overman.michael@gmail.com>
2020-08-13 22:28:55 -0600

    feat: Samodelkin: A First Kotlin Android Project
    Starter code - project creation, Big Nerd provided layout xml

---
+++ Samodelkin/app/src/main/java/tech/michaeloverman/samodelkin/NewCharacterActivity.kt
@@ -0,0 +1,11 @@
+package tech.michaeloverman.samodelkin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewCharacterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+    }
+}
